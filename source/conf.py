cf2d4b8e3f9f2802442f1ec7280db8444533b6eb
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-07-07 04:29:57 +0200

    Add logo to the documentation

--- source/conf.py
+++ source/conf.py
@@ -59,6 +59,8 @@ html_theme = "furo"
 # "<project> v<release> documentation".
 html_title = project
 
+html_logo = "cog-creators-logo.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
